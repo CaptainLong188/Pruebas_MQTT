75c25fee608ce71482be9b5262c57730854a7ffe
Cesar Giraldo <privado466@gmail.com>
2023-10-26 00:04:56 -0500

    Nueva version de PubSub

--- MQTTPubSub.py
+++ MQTTPubSub.py
@@ -1,34 +1,44 @@
 import time 
 import paho.mqtt.client as mqtt
+import random
 
 broker = "localhost"
 topic = "testTopic"
 
-# Called when the client has log information
-def on_log(client, userdata, level, buf):
-    print(f"Log : {buf}")
+def connect_mqtt():
 
-# Called when the broker responds to our connection request
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected OK")
-    else:
-        print("Bad connection")
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected OK")
+        else:
+            print("Bad connection")
 
-# Called when the client disconnects from the server
-def on_disconnect(client, userdata, rc):
-    if rc != 0:
-        print("Unexpected disconnection.")
+    def on_log(client, userdata, level, buf):
+        print(f"Log : {buf}")
 
-client = mqtt.Client("PruebaMQTT")
+    def on_disconnect(client, userdata, rc):
+        if rc != 0:
+            print("Unexpected disconnection.")
+    
+    client = mqtt.Client("PruebaMQTT")
+    client.on_connect = on_connect
+    client.on_log = on_log
+    client.on_disconnect = on_disconnect
+    client.connect(broker)
+    return client
 
-client.on_connect = on_connect
-client.on_log = on_log
-client.on_disconnect = on_disconnect
+def publish_mqtt(client):
+    # Generating random numbers as payload
+    while True: 
+        message = str(random.randint(1, 30))
+        client.publish(topic, message)
+        time.sleep(2)
 
-client.connect(broker)
-client.loop_start()
-client.publish(topic = topic, payload = 123)
-time.sleep(4)
-client.loop_stop()
-client.disconnect()
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish_mqtt(client)
+    client.loop_stop()
+
+if __name__ == '__main__':
+    run()
