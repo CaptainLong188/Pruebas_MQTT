73d17bf8c84dc2aee5abffbc10dce1b55d1faac8
Cesar Giraldo <privado466@gmail.com>
2023-10-26 10:25:46 -0500

    Se añadio decoradores

--- MQTTPubSub.py
+++ MQTTPubSub.py
@@ -7,24 +7,25 @@ topic = "testTopic"
 
 def connect_mqtt():
 
+    client = mqtt.Client("PruebaMQTT")
+
+    @client.connect_callback()
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected OK")
         else:
             print("Bad connection")
 
+    @client.log_callback()
     def on_log(client, userdata, level, buf):
         print(f"Log : {buf}")
 
+    @client.disconnect_callback()
     def on_disconnect(client, userdata, rc):
         if rc != 0:
             print("Unexpected disconnection.")
-    
-    client = mqtt.Client("PruebaMQTT")
-    client.on_connect = on_connect
-    client.on_log = on_log
-    client.on_disconnect = on_disconnect
-    client.connect(broker)
+
+    client.connect(broker, port = 1883, keepalive = 10)
     return client
 
 def publish_mqtt(client):
